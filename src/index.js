aea765efef5f3d05f4ce8c7c71757bd5bba40bcb
HNLinh <hanhatlinh26@gmail.com>
2021-04-18 17:37:50 +0700

    Firstcommit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, {PureComponent} from 'react';
+import {SocialIcon} from 'react-native-elements/dist/social/SocialIcon';
+
+export default class App extends PureComponent {
+  render() {
+    return <SocialIcon type="facebook" />;
+  }
+}

a143a31c0d8a7586c186c1f709a50098865fa5e4
Le Minh <pvlminh3005@gmail.com>
2021-06-19 11:40:35 +0700

    Update Theme

--- src/screens/tabs/NotificationScreen.js
+++ src/screens/tabs/NotificationScreen.js
@@ -1,10 +1,55 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import {
+    SafeAreaView, Container,
+    UserInfo, UserImg,
+    UserInfoWrapper, UserInfoText
+} from '../../styles/NotificationStyle'
 export default function NotificationScreen() {
     return (
-        <View style={styles.container}>
-            <Text style={{ fontSize: 22 }}>Welcome to Notification Screen</Text>
-        </View >
+        <SafeAreaView>
+            <Container>
+                <UserInfo>
+                    <UserImg source={require('../../assets/images/postImg/post1.jpg')} />
+                    <UserInfoWrapper>
+
+                        <UserInfoText>
+                            <Text style={{
+                                fontSize: 17,
+                                color: '#444',
+                                fontWeight: '700'
+                            }}>AAA </Text>
+                            đã bình luận bài viết của bạn</UserInfoText>
+                    </UserInfoWrapper>
+                </UserInfo>
+                <UserInfo>
+                    <UserImg source={require('../../assets/images/postImg/post1.jpg')} />
+                    <UserInfoWrapper>
+
+                        <UserInfoText>
+                            <Text style={{
+                                fontSize: 17,
+                                color: '#444',
+                                fontWeight: '700'
+                            }}>AAA </Text>
+                            đã bình luận bài viết của bạn</UserInfoText>
+                    </UserInfoWrapper>
+                </UserInfo>
+                <UserInfo>
+                    <UserImg source={require('../../assets/images/postImg/post1.jpg')} />
+                    <UserInfoWrapper>
+
+                        <UserInfoText>
+                            <Text style={{
+                                fontSize: 17,
+                                color: '#444',
+                                fontWeight: '700'
+                            }}>AAA </Text>
+                            đã bình luận bài viết của bạn</UserInfoText>
+                    </UserInfoWrapper>
+                </UserInfo>
+            </Container>
+        </SafeAreaView >
     )
 }
 
---
+++ src/styles/NotificationStyle.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components/native'
+
+export const SafeAreaView = styled.SafeAreaView`
+    flex: 1;
+    background: #fff;
+`
+export const Container = styled.ScrollView`
+    padding: 10px;
+    background: #fff;
+`
+
+export const UserInfo = styled.TouchableOpacity`
+    width: 100%;
+    background-color: #eee;
+    flex-direction:row;
+    padding: 5px 10px;
+    margin: 5px 0;
+`
+
+export const UserImg = styled.Image`
+    width: 60px;
+    height: 60px;
+    border-radius:30px;
+`
+export const UserInfoWrapper = styled.View`
+    margin-left: 10px;
+    justify-content: center;
+    align-items: center;
+`
+
+export const UserInfoText = styled.Text`
+    font-size: 17px;
+    color: #444;
+    font-weight: 500;
+`
+export const UserName = styled.Text`
+    font-size: 16px;
+    font-weight: bold;
+    /* font-family:'Lato-Regular'; */
+`
+
+export const PostTime = styled.Text`
+    font-size: 13px;
+    color:#666;
+    /* font-family:'Lato-Regular'; */
+`
